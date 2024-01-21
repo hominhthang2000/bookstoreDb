aaec70af1a625d702d746e5ac4f1e0ce6167ab0c
Thang Ho <hominhthang38@gmail.com>
2024-01-21 13:18:18 +0700

    commit page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" Component={Home} />
+        </Routes>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div>Dự án Book Store nhóm 4</div>;
+};
+
+export default Home;
